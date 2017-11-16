94fc74975f3df3e9d32aedb6a43ed9585b27512e
Christopher Wong <cwong164@gmail.com>
2017-11-16 16:43:58 -0600

    Preliminary backend files

---
+++ flaskcards.py
@@ -0,0 +1,22 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def login_or_redirect():
+    pass
+
+@app.route("/sets-home")
+def sets_home():
+    pass
+
+@app.route("/settings")
+def show_settings():
+    pass
+
+@app.route("/set/<int:setid>")
+def show_set(setid):
+    pass
+
+@app.route("/set/<int:setid>/edit")
+def edit_set(setid):
+    pass
