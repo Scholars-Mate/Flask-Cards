0aa90a092139bea9e4f311829f6b988f32441d6c
Roger Kiew <yuichiu416@gmail.com>
2017-11-25 13:04:50 -0600

    Added skeletons for helper functions

--- helpers.py
+++ helpers.py
@@ -31,3 +31,106 @@ def logIn(username: str, password: str) -> bool:
     # Set session values and return
     session['userid'] = result[0]
     return(True)
+
+def getSets(userid:int):
+    """Take the userid, display all sets in their account
+
+    Returns the sets as a list of dictionaries with the following keys:
+    'id'
+    'name'
+    'category'
+    """
+    pass
+
+def getCards(setid:int):
+    """Get all the cards in the given set
+
+    Returns the cards as a list of dictionaries with the following keys:
+    'id'
+    'front'
+    'back'
+    'indicator'
+    """
+    pass
+
+def addCard(cardFront:str, cardBack:str, setid:int) -> bool:
+    """Add a new card to the given set
+
+    Uses the given information, and the userid in the session, and adds a card
+    to the given set
+    If the card was successfully added, return true, else return false
+    """
+    pass
+
+def editCard(cardid:int, cardFront:str, cardBack:str) -> bool:
+    """Modify a card
+
+    If successfully edited, return true, else return false
+    """
+    pass
+
+def deleteCard(cardid:id) -> bool:
+    """Delete a card
+
+    If successfully deleted, return true, else return false
+    """
+    pass
+
+def indicateCard(cardid:int, indicator:bool = False) -> bool:
+    """Set the indicator to True or False
+
+    If successfully changed it's value, return true, else return false
+    """
+    pass
+
+def addCategory(categoryName:str) -> bool:
+    """Add a new category
+
+    Create a new category with the given name, and the userid from the session
+    If successfully added, return true, else return false
+    """
+    pass
+
+def addSet(setName:str, categoryid:int) -> bool:
+    """Add a new set
+
+    Create an empty set with the given name and categoryid, as well as the
+    userid from the session
+    If successful, return true, else return false
+    """
+    pass
+
+def modifySet(setid:int, newName:str) -> bool:
+    """Change a set's name
+
+    If successful, return true, else return false
+    """
+    pass
+
+def modifyCategory(categoryid:int, newName:str) -> bool:
+    """Change a category's name
+
+    If successful, return true, else return false
+    """
+    pass
+
+def deleteSets(setid:int) -> bool:
+    """Delete a set and all cards belonging to it
+
+    If successful, return true, else return false
+    """
+    pass
+
+def deleteCategory(categoryid: int) -> bool:
+    """Delete a category and all sets belonging to it
+
+    If successful, return true, else return false
+    """
+    pass
+
+def signOut() -> bool:
+    """Sign out the user
+
+    If the user was successfully signed out, return true, else return false
+    """
+    pass
