4174c9960107b41a8d4507b17ad96dde843196f2
kawaiiastronaut <chases1997@yahoo.com>
2017-12-05 15:23:37 -0600

    Moved experimental functions from fork

--- flaskcards.py
+++ flaskcards.py
@@ -1,22 +1,32 @@
 from flask import Flask
 app = Flask(__name__)
-
+    import helpers.py 
+    import database.py
 @app.route("/")
 def login_or_redirect():
-    pass
+    user = input('Enter userId: ');
+    passw = input('Enter password: ');
+    return logIn(user,passw);
 
 @app.route("/sets-home")
 def sets_home():
-    pass
+    user = input('Enter userId: ');
+    return getSets(user);
+    
 
 @app.route("/settings")
 def show_settings():
+    
     pass
 
 @app.route("/set/<int:setid>")
 def show_set(setid):
-    pass
+    return getCards(setid);
 
 @app.route("/set/<int:setid>/edit")
 def edit_set(setid):
-    pass
+    name = input('Enter new set name: ');
+    if(modifySet(setid,name))
+    return true;
+    else
+    return false;
