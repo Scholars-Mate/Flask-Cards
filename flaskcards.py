996daf6d7e4bd223a942a417fb647dd964885e15
Christopher Wong <cwong164@gmail.com>
2017-12-07 23:15:22 -0600

    Added routes for main pages

--- flaskcards.py
+++ flaskcards.py
@@ -1,22 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, request
+import helpers
 app = Flask(__name__)
 
 @app.route("/")
-def login_or_redirect():
-    pass
+def homepage_or_redirect():
+    if session.get('userid') is None:
+        return redirect(url_for('login'))
+    sets = helpers.getSets(session.get('userid'))
+    return render_template('Homepage.html', sets=sets)
 
-@app.route("/sets-home")
-def sets_home():
-    pass
-
-@app.route("/settings")
-def show_settings():
-    pass
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        if helpers.logIn(request.form['username'], request.form['password']):
+            return redirect(url_for('homepage_or_redirect'))
+        return render_template('login.html', login_fail=True)
+    else:
+        return render_template('login.html')
 
 @app.route("/set/<int:setid>")
 def show_set(setid):
-    pass
+    if session.get('userid') is None:
+        return redirect(url_for('login'))
 
-@app.route("/set/<int:setid>/edit")
-def edit_set(setid):
-    pass
+    cards = helpers.getCards(setid)
+    return render_template('cardPreview.html', cards=cards, setid=setid)
+
+app.secret_key = ''
